05b3eaefca5e69eb2e1827e67c59b4c9ba98665d
YUERONG HUANG <rowanne.h@hotmail.com>
2024-02-13 00:12:07 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body:JSON.stringify({
+      name: name,
+      email: email,
+    })
+  })
+  .then(res => res.json())
+  .then(user => {
+    document.body.innerHTML = user.id;
+  })
+  .catch(err => {
+    document.body.innerHTML = err.message;
+  })
+}
